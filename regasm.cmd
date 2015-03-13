d425fb67d16cfc71da78b65f3006f3bfe805f3e6
virghy <virginiogonzalez@gmail.com>
2015-03-13 15:29:46 -0300

    Version Inicial

---
+++ regasm.cmd
@@ -0,0 +1,3 @@
+"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm" C:\Sistemas\Futura9\FuturaCOM.DLL
+"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm" C:\Sistemas\Futura9\NET4COM.DLL
+pause
